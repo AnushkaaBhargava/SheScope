8b8a06e1fa53da1be52a0e4127965054cd7dc3f5
Anushkaa Bhargava <anushkaab2005@gmail.com>
2026-07-09 18:53:26 +0530

    JWT authentication done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,14 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-const registerUser= async(req,res)=>{
+export const registerUser= async(req,res)=>{
     try{
        
+      
        const {name,email,password,phone,branch,year,cgpa}=req.body;
-
+        const hashedPassword=await bcrypt.hash(password,5);
        const existingUser=await User.findOne({email});
 
        if(existingUser){
@@ -16,7 +20,7 @@ const registerUser= async(req,res)=>{
        const user=await User.create({
         name,
         email,
-        password,
+        password:hashedPassword,
         phone,
         branch,
         year,
@@ -34,4 +38,44 @@ const registerUser= async(req,res)=>{
     }
 };
 
-export default registerUser;
+export const loginUser=async(req,res)=>{
+       try{
+          const {email,password}=req.body;
+          const user=await User.findOne({email});
+          if(!user){
+            return res.status(400).json({
+                message:"Invalid email or password"
+            });
+          }
+
+          const isMatch=await bcrypt.compare(password,user.password);
+
+          if(!isMatch){
+             return res.status(400).json({
+                message: "Invalid email or password"
+            });
+          }
+
+          const token=jwt.sign({
+            id:user._id,
+            role:user.role
+
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn:"7d"
+          });
+
+             res.status(200).json({
+            message: "Login successful",
+            token,
+            user
+        });
+       }
+       catch(error){
+          
+         res.status(500).json({
+            message: error.message
+        });
+       }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import registerUser from "../controllers/authController.js";
-
+import { registerUser, loginUser } from "../controllers/authController.js";
+import protect from "../middleware/authMiddleware.js";
 const router=express.Router();
 
 router.post("/register",registerUser);
+
+router.post("/login",loginUser);
+
+router.get("/profile",protect,(req,res)=>{
+    res.json({
+        message:"Protected Route",
+        user:req.user
+    });
+});
 
 export default router;
