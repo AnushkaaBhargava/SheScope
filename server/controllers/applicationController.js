b9cf47a77d865107eb6d84de260f9ecb4727ddc3
Anushkaa Bhargava <anushkaab2005@gmail.com>
2026-07-10 17:27:33 +0530

    Application module completed

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -6,6 +6,17 @@ export const applyScholarship= async(req,res)=>{
 
         const {scholarship,sop,resume}=req.body;
 
+        const existingApplication=await Application.findOne({
+              student:req.user.id,
+              scholarship
+        })
+
+        if(existingApplication){
+            return res.json({
+                message:"You have already applied to this scholarship!"
+            })
+        }
+
         const application=await Application.create({
             student:req.user.id,
             scholarship,
@@ -42,6 +53,55 @@ export const getMyApplications= async (req,res)=>{
         res.status(500).json({
             message:error.message
         });
+
+    }
+}
+
+export const getAllApplications=async (req,res)=>{
+    try{
+
+        const applications=await Application.find()
+        .populate("student","name email")
+        .populate("scholarship","title company");
+
+        res.status(200).json({applications});
+
+    }catch(error){
+
+        res.status(500).json({
+             message:error.message
+        });
+
+    }
+}
+
+export const updateApplicationStatus= async(req,res)=>{
+    try{
+        
+        const {status}=req.body;
+
+        const application=await Application.findByIdAndUpdate(
+            req.params.id,
+            {status},
+            {returnDocument:"after"}
+        );
+
+        if (!application) {
+            return res.status(404).json({
+                message: "Application not found!"
+            });
+        }
+
+        res.status(200).json({
+            message:"Application status updated successfully!",
+            application
+        });
+    }catch(error){
+
+         res.status(500).json({
+            message: error.message
+        });
+
 
     }
 }
--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import {applyScholarship,getMyApplications} from "../controllers/applicationController.js";
+import {applyScholarship,getMyApplications, getAllApplications,updateApplicationStatus} from "../controllers/applicationController.js";
 import protect from "../middleware/authMiddleware.js";
+import admin from "../middleware/adminMiddleware.js";
 
 const router=express.Router();
 
 router.post("/",protect,applyScholarship);
 
 router.get("/my",protect,getMyApplications);
+
+router.get("/",protect,admin,getAllApplications);
+
+router.put("/:id",protect,admin,updateApplicationStatus);
 
 export default router;
